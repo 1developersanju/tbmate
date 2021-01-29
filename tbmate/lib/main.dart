eb946b408381fd2e1cb59d9b25424c108d0bc84e
DeveloperSanju <sanjuwings04@gmail.com>
2021-01-29 11:38:26 +0530

    Init

--- tbmate/lib/main.dart
+++ tbmate/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'scan.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MaterialApp(
     home: MyApp(),
   ));
--- tbmate/lib/play.dart
+++ tbmate/lib/play.dart
@@ -1,9 +1,111 @@
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:velocity_x/velocity_x.dart';
+// import 'package:aeyrium_sensor/aeyrium_sensor.dart';
+// import 'package:web_socket_channel/web_socket_channel.dart';
+// import 'package:web_socket_channel/io.dart';
+// import 'dart:async';
+
+// double x = 0.0, y = 0.0;
+
+// bool i = true;
+// String schannel;
+
+// class Gacce extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: MyHomePage(
+//         channel: IOWebSocketChannel.connect('wss://tbmatekl.com/wss2/NNN'),
+//       ),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   final WebSocketChannel channel;
+//   MyHomePage({Key key, @required this.channel}) : super(key: key);
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   StreamSubscription<dynamic> _streamSubscriptions;
+
+//   @override
+//   void initState() {
+//     while (i) {
+//       print("subscribing..");
+//       widget.channel.sink.add('{"command":"subscribe", "channel":"$schannel"}');
+//       i = false;
+//     }
+//     print("messaging..");
+
+//     _streamSubscriptions = AeyriumSensor.sensorEvents.listen((event) {
+//       setState(() {
+//         x = event.roll * -2.5;
+//         y = event.pitch * 2.5;
+//       });
+
+//       // x = x - 5.5;
+//       // y = y + 3.8;
+//       // print('$x  $y');
+//       for (var i = 3000; i > 0; i--) {}
+
+//       widget.channel.sink.add(
+//           '{"command": "message", "message": "${x.toStringAsFixed(1)},${y.toStringAsFixed(1)}"}');
+//     });
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       body: Container(
+//           child: VStack([
+//         Image(image: AssetImage('assets/mobileIcon.jpeg')).centered(),
+//         Padding(padding: EdgeInsets.only(top: 62)),
+//         "Slowly tilt & pan your phone to search"
+//             .text
+//             .size(18)
+//             .color(Colors.grey)
+//             .makeCentered(),
+//         "the sound of Kuala Lampur."
+//             .text
+//             .size(18)
+//             .color(Colors.grey)
+//             .makeCentered(),
+//         Padding(padding: EdgeInsets.only(top: 20)),
+//         "Guess the location by listening to it."
+//             .text
+//             .size(18)
+//             .color(Colors.lightBlue[100])
+//             .makeCentered(),
+//         Padding(padding: EdgeInsets.only(top: 20)),
+//         "${x.toStringAsFixed(1)}  ${y.toStringAsFixed(1)}"
+//             .text
+//             .size(18)
+//             .color(Colors.lightBlue[100])
+//             .makeCentered(),
+//       ])).centered(),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     widget.channel.sink.close();
+//     super.dispose();
+//   }
+// }
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:aeyrium_sensor/aeyrium_sensor.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/io.dart';
+import 'package:sensors/sensors.dart';
 import 'dart:async';
 
 double x = 0.0, y = 0.0;
@@ -32,6 +134,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   StreamSubscription<dynamic> _streamSubscriptions;
+  List<StreamSubscription<dynamic>> _streamSubscriptions2 =
+      <StreamSubscription<dynamic>>[];
 
   @override
   void initState() {
@@ -41,21 +145,19 @@ class _MyHomePageState extends State<MyHomePage> {
       i = false;
     }
     print("messaging..");
-
-    _streamSubscriptions = AeyriumSensor.sensorEvents.listen((event) {
+    _streamSubscriptions2
+        .add(accelerometerEvents.listen((AccelerometerEvent event) {
       setState(() {
-        x = event.roll * -2.5;
-        y = event.pitch * 2.5;
+        x = event.x * -2.0;
+        y = event.y * -2.0;
       });
 
       // x = x - 5.5;
       // y = y + 3.8;
       // print('$x  $y');
-      for (var i = 3000; i > 0; i--) {}
-
       widget.channel.sink.add(
           '{"command": "message", "message": "${x.toStringAsFixed(1)},${y.toStringAsFixed(1)}"}');
-    });
+    }));
     super.initState();
   }
 
